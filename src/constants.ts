b4f7d8da902719e65f12079b3ffef653e9bff67b
Роман Савощик <tweakice@gmail.com>
2023-03-13 15:23:15 +0600

    add constants

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const METACORE_ADDRESS = '0xE7e4f9973c3F46FcAdAbF504D3Bbcf874B447eFa';
+export const METAFORCE_ADDRESS = '0x47c58e0d5503ec4ac9474fdce74ebeac71a56592';
+export const ROOT_ID = 1;
+export const LEVELS = 9;
