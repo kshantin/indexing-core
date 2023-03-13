0c99fa2342f9c78c563789eb37e502c39af608f4
Роман Савощик <tweakice@gmail.com>
2023-03-13 15:48:06 +0600

    add processor context

--- src/processor.ts
+++ src/processor.ts
@@ -1,63 +1,79 @@
-import { TypeormDatabase } from '@subsquid/typeorm-store';
-import {EvmBatchProcessor} from '@subsquid/evm-processor'
-import { lookupArchive } from '@subsquid/archive-registry'
-import assert from 'assert';
-import { Burn } from './model';
+import { TypeormDatabase } from "@subsquid/typeorm-store";
+import { EvmBatchProcessor } from "@subsquid/evm-processor";
+import {
+  METACORE_ADDRESS,
+  METAFORCE_ADDRESS,
+  ROOT_ID,
+  LEVELS,
+} from "./constants";
+import * as core from "./abi/core";
+import * as metaForce from "./abi/meta-force";
+import {
+  Event,
+  LostMoney,
+  MarketingReferrerChanged,
+  Pack,
+  RevenueMFS,
+  RevenueStable,
+  TimestampEndPack,
+  User,
+} from "./model";
+import { EntityManager } from "typeorm";
+import { BigNumber } from "ethers";
+import { lookupArchive } from "@subsquid/archive-registry";
 
+const database = new TypeormDatabase();
 const processor = new EvmBatchProcessor()
+  .setBlockRange({ from: 31461665 })
   .setDataSource({
-    // uncomment and set RPC_ENDPOONT to enable contract state queries. 
-    // Both https and wss endpoints are supported. 
+    // uncomment and set RPC_ENDPOONT to enable contract state queries.
+    // Both https and wss endpoints are supported.
     // chain: process.env.RPC_ENDPOINT,
 
-    // Change the Archive endpoints for run the squid 
+    // Change the Archive endpoints for run the squid
     // against the other EVM networks
     // For a full list of supported networks and config options
     // see https://docs.subsquid.io/develop-a-squid/evm-processor/configuration/
 
-    archive: lookupArchive('eth-mainnet'),
+    archive: lookupArchive("polygon-mumbai") || "https://polygon-mumbai.archive.subsquid.io",
   })
-  .addTransaction([
-    '0x0000000000000000000000000000000000000000'
-  ], {
-    range: {
-      from: 6_000_000
-    },
+  .addLog(METACORE_ADDRESS, {
+    filter: [
+      [
+        core.events.MarketingReferrerChanged.topic,
+        core.events.TimestampEndPackSet.topic,
+      ],
+    ],
     data: {
+      evmLog: {
+        topics: true,
+        data: true,
+      },
       transaction: {
-        from: true,
-        value: true,
-        hash: true
-      }
-    }
+        hash: true,
+      },
+    },
+  })
+  .addLog(METAFORCE_ADDRESS, {
+    filter: [
+      [
+        metaForce.events.RevenueMFS.topic,
+        metaForce.events.RevenueStable.topic,
+        metaForce.events.LostMoney.topic,
+      ],
+    ],
+    data: {
+      evmLog: {
+        topics: true,
+        data: true,
+      },
+      transaction: {
+        hash: true,
+      },
+    },
   });
 
-function formatID(height:any, hash:string) {
-  return `${String(height).padStart(10, '0')}-${hash}` 
-} 
-
-processor.run(new TypeormDatabase(), async (ctx) => {
-  const burns: Burn[] = []
-  for (let c of ctx.blocks) {
-    for (let i of c.items) {
-      assert(i.kind == 'transaction')
-      // decode and normalize the tx data
-      burns.push(new Burn({
-        id: formatID(c.header.height, i.transaction.hash),
-        block: c.header.height,
-        address: i.transaction.from,
-        value: i.transaction.value,
-        txHash: i.transaction.hash
-      }))
-    }
-   }
-   // apply vectorized transformations and aggregations
-   const burned = burns.reduce((acc, b) => acc + b.value, 0n)/1_000_000_000n
-   const startBlock = ctx.blocks.at(0)?.header.height
-   const endBlock = ctx.blocks.at(-1)?.header.height
-   ctx.log.info(`Burned ${burned} Gwei from ${startBlock} to ${endBlock}`)
-
-   // upsert batches of entities with batch-optimized ctx.store.save
-   await ctx.store.save(burns)
+processor.run(database, async (ctx) => {
+  
 });
 
