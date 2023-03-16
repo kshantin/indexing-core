8d73f5d907a39baa8c222cdb5da4b5e9d1515fcc
Роман Савощик <tweakice@gmail.com>
2023-03-16 16:01:28 +0600

    solves problem with indexing Core

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const CORE_PROXY_ADDRESS = '0xE7e4f9973c3F46FcAdAbF504D3Bbcf874B447eFa';
+export const CORE_PROXY_ADDRESS = '0xe7e4f9973c3f46fcadabf504d3bbcf874b447efa';
 export const METAFORCE_PROXY_ADDRESS = '0x47c58e0d5503ec4ac9474fdce74ebeac71a56592';
 // From contract:
 // uint256 constant LEVELS_COUNT = 9;
